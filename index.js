c4c14a7ae91bdb831a3ab5b6160888f673ab3b2c
Manav Chawla <chawlamanav428@gmail.com>
2024-02-17 12:59:03 +0530

    fixed issue

--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const stateslist = document.getElementById("states");
 fetch("https://api.jsonbin.io/v3/b/65d05a9edc74654018a5f0ab")
   .then((response) => response.json())
   .then((data) => {
-    data.record.forEach((countryData) => {
+    data.record.record.forEach((countryData) => {
       const countrybutton = document.createElement("button");
       countrybutton.textContent = countryData.country;
       buttons.appendChild(countrybutton);
-
       countrybutton.addEventListener("click", () => {
         // Clear previous state list
         stateslist.innerHTML = "";
-
         // Iterate over the states of the selected country
         countryData.states.map((state) => {
           const stateItem = document.createElement("li");
